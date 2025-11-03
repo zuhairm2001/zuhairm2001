3aae35063d3b5f8ee80d8f80defd7bc7a4018c01
zuhairm2001 <zuhairm2001@hotmail.com>
2025-11-04 00:14:24 +1100

    added s3 api for resume upload to ts script, started working on workflow file for auto pushing resume to bucket on commit when its changed

---
+++ internal/s3/main.go
@@ -0,0 +1,5 @@
+package s3
+
+//in this module we will just grab the presigned link and return it
+
+
--- resume-generator/generate-pdf.ts
+++ resume-generator/generate-pdf.ts
@@ -1,10 +1,17 @@
 import fs from "fs";
 import { marked } from "marked";
 import puppeteer from "puppeteer";
+import {
+  S3Client,
+  ListBucketsCommand,
+  ListObjectsCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 
 const markdown = fs.readFileSync("resume.md", "utf-8");
 const htmlContent = marked(markdown);
-const css = fs.readFileSync("resume.css")
+const css = fs.readFileSync("resume.css");
 
 const fullHTML = `
 <!DOCTYPE html>
@@ -27,8 +34,28 @@ const fullHTML = `
   await page.pdf({
     path: "output.pdf",
     format: "A4",
-    printBackground: true
+    printBackground: true,
   });
   await browser.close();
   console.log("PDF generated: output.pdf");
 })();
+
+const S3 = new S3Client({
+  region: "auto",
+  endpoint: Bun.env.S3_ENDPOINT!,
+  credentials: {
+    accessKeyId: Bun.env.S3_ACCESS_KEY!,
+    secretAccessKey: Bun.env.S3_SECRET_KEY!,
+  },
+});
+
+await S3.send(new PutObjectCommand({
+  Bucket: "personal-bucket",
+  Key: "resume.pdf",
+  Body: fs.readFileSync("output.pdf"),
+  ContentType: "application/pdf",
+}));
+
+console.log(
+  await S3.send(new ListObjectsCommand({ Bucket: "personal-bucket" })),
+);
